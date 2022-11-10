41cbbf32ffd3d42a7345ae71e93dc2715c53bbc2
Doruk Ozturk <dozturk@amazon.com>
2022-11-10 14:26:21 -0500

    Add test for cluster private endpoint

--- tests/test_security_iam.py
+++ tests/test_security_iam.py
@@ -1,9 +1,22 @@
+import json
+from pathlib import Path
+from unittest.mock import patch
+
 import pytest
+
+from hardeneks.resources import NamespacedResources
 
 from hardeneks.security.iam import (
     restrict_wildcard_for_roles,
     restrict_wildcard_for_cluster_roles,
+    check_endpoint_public_access,
 )
+
+
+def read_json(file_path):
+    with open(file_path) as f:
+        json_content = json.load(f)
+    return json_content
 
 
 @pytest.mark.parametrize(
@@ -28,3 +41,23 @@ def test_restrict_wildcard_for_cluster_roles(namespaced_resources):
 
     assert "good" not in [i.metadata.name for i in offenders]
     assert "bad" in [i.metadata.name for i in offenders]
+
+
+@patch("boto3.client")
+def test_check_endpoint_public_access(mocked_client):
+    namespaced_resources = NamespacedResources(
+        "some_region", "some_context", "some_cluster", "some_ns"
+    )
+
+    test_data = (
+        Path.cwd()
+        / "tests"
+        / "data"
+        / "check_endpoint_public_access"
+        / "cluster_metadata.json"
+    )
+
+    mocked_client.return_value.describe_cluster.return_value = read_json(
+        test_data
+    )
+    assert check_endpoint_public_access(namespaced_resources)
