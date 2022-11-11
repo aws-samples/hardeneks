eea5ce806f9355012d9cf3ab2024d7a98391c01b
Doruk Ozturk <dozturk@amazon.com>
2022-11-11 13:20:29 -0500

    Add test cases for pod security

---
+++ hardeneks/security/pod_security.py
@@ -0,0 +1,103 @@
+from rich.console import Console
+
+from ..report import (
+    print_pod_table,
+)
+from ..resources import NamespacedResources
+
+
+console = Console()
+
+
+def disallow_container_socket_mount(namespaced_resources: NamespacedResources):
+    offenders = []
+
+    sockets = [
+        "/var/run/docker.sock",
+        "/var/run/containerd.sock",
+        "/var/run/crio.sock",
+    ]
+
+    for pod in namespaced_resources.pods:
+        for volume in pod.spec.volumes:
+            if volume.host_path and volume.host_path.path in sockets:
+                offenders.append(pod)
+
+    if offenders:
+        print_pod_table(offenders, "Container socket mounts are not allowed")
+
+    return offenders
+
+
+def disallow_host_path_or_make_it_read_only(
+    namespaced_resources: NamespacedResources,
+):
+    offenders = []
+
+    for pod in namespaced_resources.pods:
+        for volume in pod.spec.volumes:
+            if volume.host_path:
+                offenders.append(pod)
+
+    if offenders:
+        print_pod_table(
+            offenders,
+            "Restrict the use of hostpath.",
+        )
+
+    return offenders
+
+
+def set_requests_limits_for_containers(
+    namespaced_resources: NamespacedResources,
+):
+    offenders = []
+
+    for pod in namespaced_resources.pods:
+        for container in pod.spec.containers:
+            if not (
+                container.resources.limits and container.resources.requests
+            ):
+                offenders.append(pod)
+
+    if offenders:
+        print_pod_table(
+            offenders,
+            "Set requests and limits for each container.",
+        )
+
+    return offenders
+
+
+def disallow_privilege_escalation(namespaced_resources: NamespacedResources):
+    offenders = []
+
+    for pod in namespaced_resources.pods:
+        for container in pod.spec.containers:
+            if container.security_context.allow_privilege_escalation:
+                offenders.append(pod)
+
+    if offenders:
+        print_pod_table(
+            offenders, "Set allowPrivilegeEscalation in the pod spec to false"
+        )
+
+    return offenders
+
+
+def check_read_only_root_file_system(
+    namespaced_resources: NamespacedResources,
+):
+    offenders = []
+
+    for pod in namespaced_resources.pods:
+        for container in pod.spec.containers:
+            if container.security_context.read_only_root_filesystem:
+                offenders.append(pod)
+    if offenders:
+        print_pod_table(
+            offenders,
+            "Configure your images with a read-only root file system",
+        )
+
+    return offenders
---
+++ tests/test_security_pod_security.py
@@ -0,0 +1,67 @@
+import pytest
+
+from hardeneks.security.pod_security import (
+    disallow_container_socket_mount,
+    disallow_host_path_or_make_it_read_only,
+    set_requests_limits_for_containers,
+    disallow_privilege_escalation,
+    check_read_only_root_file_system,
+)
+
+
+@pytest.mark.parametrize(
+    "namespaced_resources",
+    [("disallow_container_socket_mount")],
+    indirect=["namespaced_resources"],
+)
+def test_disallow_container_socket_mount(namespaced_resources):
+    offenders = disallow_container_socket_mount(namespaced_resources)
+
+    assert "bad" in [i.metadata.name for i in offenders]
+
+
+@pytest.mark.parametrize(
+    "namespaced_resources",
+    [("disallow_host_path_or_make_it_read_only")],
+    indirect=["namespaced_resources"],
+)
+def test_disallow_host_path_or_make_it_read_only(namespaced_resources):
+    offenders = disallow_host_path_or_make_it_read_only(namespaced_resources)
+
+    assert "bad" in [i.metadata.name for i in offenders]
+
+
+@pytest.mark.parametrize(
+    "namespaced_resources",
+    [("set_requests_limits_for_containers")],
+    indirect=["namespaced_resources"],
+)
+def test_set_requests_limits_for_containers(namespaced_resources):
+    offenders = set_requests_limits_for_containers(namespaced_resources)
+
+    assert "good" not in [i.metadata.name for i in offenders]
+    assert "bad" in [i.metadata.name for i in offenders]
+
+
+@pytest.mark.parametrize(
+    "namespaced_resources",
+    [("disallow_privilege_escalation")],
+    indirect=["namespaced_resources"],
+)
+def test_disallow_privilege_escalation(namespaced_resources):
+    offenders = disallow_privilege_escalation(namespaced_resources)
+
+    assert "good" not in [i.metadata.name for i in offenders]
+    assert "bad" in [i.metadata.name for i in offenders]
+
+
+@pytest.mark.parametrize(
+    "namespaced_resources",
+    [("check_read_only_root_file_system")],
+    indirect=["namespaced_resources"],
+)
+def test_check_read_only_root_file_system(namespaced_resources):
+    offenders = check_read_only_root_file_system(namespaced_resources)
+
+    assert "good" not in [i.metadata.name for i in offenders]
+    assert "bad" in [i.metadata.name for i in offenders]
