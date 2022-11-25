4caed90d31e8e22c23472c8f9af8e952bef7ce86
Doruk Ozturk <dozturk@amazon.com>
2022-11-24 22:24:39 -0500

    Add tests for psa at namespace level

---
+++ hardeneks/cluster_wide/security/pod_security.py
@@ -0,0 +1,27 @@
+import kubernetes
+
+from ...resources import Resources
+
+from ...report import (
+    print_namespace_table,
+)
+
+
+def ensure_namespace_psa_exist(resources: Resources):
+    offenders = []
+    namespaces = kubernetes.client.CoreV1Api().list_namespace().items
+    for namespace in namespaces:
+        if namespace.metadata.name not in resources.namespaces:
+            labels = namespace.metadata.labels.keys()
+            if "pod-security.kubernetes.io/enforce" not in labels:
+                offenders.append(namespace.metadata.name)
+            elif "pod-security.kubernetes.io/warn" not in labels:
+                offenders.append(namespace.metadata.name)
+
+    if offenders:
+        print_namespace_table(
+            offenders,
+            "[red]Namespaces should have psa modes.",
+        )
+
+    return offenders
