a821ae7f7ab69dffcff4768a94a3f57e9b2a8ee7
Doruk Ozturk <dozturk@amazon.com>
2022-11-27 11:00:23 -0500

    Implement checks for secrets as env

--- tests/test_security_encryption_secrets.py
+++ tests/test_security_encryption_secrets.py
@@ -5,6 +5,9 @@ from hardeneks.cluster_wide.security.encryption_secrets import (
     use_encryption_with_efs,
     use_efs_access_points,
 )
+from hardeneks.namespace_based.security.encryption_secrets import (
+    disallow_secrets_from_env_vars,
+)
 
 
 @pytest.mark.parametrize(
@@ -38,6 +41,18 @@ def test_use_encryption_with_efs(resources):
 )
 def test_use_efs_access_points(resources):
     offenders = use_efs_access_points(resources)
+
+    assert "good" not in [i.metadata.name for i in offenders]
+    assert "bad" in [i.metadata.name for i in offenders]
+
+
+@pytest.mark.parametrize(
+    "namespaced_resources",
+    [("disallow_secrets_from_env_vars")],
+    indirect=["namespaced_resources"],
+)
+def test_disallow_secrets_from_env_vars(namespaced_resources):
+    offenders = disallow_secrets_from_env_vars(namespaced_resources)
 
     assert "good" not in [i.metadata.name for i in offenders]
     assert "bad" in [i.metadata.name for i in offenders]
