74a602be73347faab537128ffa7998a02c85787a
Doruk Ozturk <dozturk@amazon.com>
2022-11-27 23:52:37 -0500

    Implement checks for amazon inspector

--- hardeneks/cluster_wide/security/infrastructure_security.py
+++ hardeneks/cluster_wide/security/infrastructure_security.py
@@ -1,7 +1,13 @@
 import boto3
+from rich.console import Console
+from rich.panel import Panel
+from rich import print
 
 from ...resources import Resources
 from ...report import print_instance_public_table
+
+
+console = Console()
 
 
 def deploy_workers_onto_private_subnets(resources: Resources):
@@ -30,3 +36,25 @@ def deploy_workers_onto_private_subnets(resources: Resources):
             "[red]Place worker nodes on private subnets.",
         )
     return offenders
+
+
+def make_sure_inspector_is_enabled(resources: Resources):
+    client = boto3.client("inspector2")
+    account_id = boto3.client("sts").get_caller_identity()["Account"]
+
+    response = client.batch_get_account_status(
+        accountIds=[
+            account_id,
+        ]
+    )
+
+    resource_state = response["accounts"][0]["resourceState"]
+    ec2_status = resource_state["ec2"]["status"]
+    ecr_status = resource_state["ecr"]["status"]
+
+    if ec2_status != "ENABLED" and ecr_status != "ENABLED":
+        print(Panel("[red]Enable Amazon Inspector for ec2 and ecr"))
+        console.print()
+        return False
+
+    return True
