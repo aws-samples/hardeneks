23ce94a7e8a32f703591c3790969f268f40a2a68
Doruk Ozturk <dozturk@amazon.com>
2022-11-27 23:52:37 -0500

    Implement checks for amazon inspector

--- tests/test_security_infrastructure_security.py
+++ tests/test_security_infrastructure_security.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 
 from hardeneks.cluster_wide.security.infrastructure_security import (
     deploy_workers_onto_private_subnets,
+    make_sure_inspector_is_enabled,
 )
 from hardeneks.resources import Resources
 
@@ -12,6 +13,10 @@ def read_json(file_path):
     with open(file_path) as f:
         json_content = json.load(f)
     return json_content
+
+
+def mocked_caller_identity():
+    return {"Account": "foo"}
 
 
 @patch("boto3.client")
@@ -38,3 +43,25 @@ def test_deploy_workers_onto_private_subnets(mocked_client):
     assert "i-083cc9da5e18e2702" not in instance_ids
     assert "i-01c10da9688b958a0" in instance_ids
     assert "i-0f282d6ee7edb633f" in instance_ids
+
+
+@patch("boto3.client")
+def test_make_sure_inspector_is_enabled(mocked_client):
+    namespaced_resources = Resources(
+        "some_region", "some_context", "some_cluster", "some_ns"
+    )
+
+    test_data = (
+        Path.cwd()
+        / "tests"
+        / "data"
+        / "make_sure_inspector_is_enabled"
+        / "inspector_status.json"
+    )
+
+    mocked_client.return_value.batch_get_account_status.return_value = (
+        read_json(test_data)
+    )
+    mocked_client.return_value.get_caller_identity = mocked_caller_identity
+
+    assert not make_sure_inspector_is_enabled(namespaced_resources)
