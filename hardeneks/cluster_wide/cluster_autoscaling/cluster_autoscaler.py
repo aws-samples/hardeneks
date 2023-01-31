6fea9b249848479013640384ee62d34d935225b4
Doruk Ozturk <dozturk@amazon.com>
2023-01-31 12:24:18 -0500

    feat: Add check for CA-k8s version mismatch

--- hardeneks/cluster_wide/cluster_autoscaling/cluster_autoscaler.py
+++ hardeneks/cluster_wide/cluster_autoscaling/cluster_autoscaler.py
@@ -1,3 +1,4 @@
+import boto3
 from kubernetes import client
 from rich.panel import Panel
 
@@ -23,3 +24,30 @@ def check_any_cluster_autoscaler_exists(resources: Resources):
         return False
     else:
         return True
+
+
+def ensure_cluster_autoscaler_and_cluster_versions_match(resources: Resources):
+
+    eks_client = boto3.client("eks", region_name=resources.region)
+    cluster_metadata = eks_client.describe_cluster(name=resources.cluster)
+
+    cluster_version = cluster_metadata["cluster"]["version"]
+
+    deployments = client.AppsV1Api().list_deployment_for_all_namespaces().items
+
+    for deployment in deployments:
+        if deployment.metadata.name == "cluster-autoscaler":
+            ca_containers = deployment.spec.template.spec.containers
+            ca_image = ca_containers[0].image
+            ca_image_version = ca_image.split(":")[-1]
+            if cluster_version not in ca_image_version:
+                console.print(
+                    Panel(
+                        f"[red]CA({ca_image_version})-k8s({cluster_version}) Cross version compatibility is not recommended.",
+                        subtitle="[link=https://aws.github.io/aws-eks-best-practices/cluster-autoscaling/#operating-the-cluster-autoscaler]Click to see the guide[/link]",
+                    )
+                )
+                console.print()
+                return False
+            else:
+                return True
--- tests/test_cluster_autoscaling_cluster_autoscaler.py
+++ tests/test_cluster_autoscaling_cluster_autoscaler.py
@@ -6,6 +6,7 @@ import kubernetes
 from hardeneks.resources import Resources
 from hardeneks.cluster_wide.cluster_autoscaling.cluster_autoscaler import (
     check_any_cluster_autoscaler_exists,
+    ensure_cluster_autoscaler_and_cluster_versions_match,
 )
 from .conftest import get_response
 
@@ -29,3 +30,31 @@ def test_check_any_cluster_autoscaler_exists(mocked_client):
     resources = Resources("some_region", "some_context", "some_cluster", [])
 
     assert not check_any_cluster_autoscaler_exists(resources)
+
+
+@patch("boto3.client")
+@patch("kubernetes.client.AppsV1Api.list_deployment_for_all_namespaces")
+def test_ensure_cluster_autoscaler_and_cluster_versions_matchs(
+    mocked_k8s_client, mocked_boto_client
+):
+
+    test_data = (
+        Path.cwd()
+        / "tests"
+        / "data"
+        / "ensure_cluster_autoscaler_and_cluster_versions_match"
+        / "cluster"
+        / "deployments_api_response.json"
+    )
+    mocked_k8s_client.return_value = get_response(
+        kubernetes.client.AppsV1Api,
+        test_data,
+        "V1DeploymentList",
+    )
+
+    mocked_boto_client.return_value.describe_cluster.return_value = {
+        "cluster": {"version": "1.23"}
+    }
+    resources = Resources("some_region", "some_context", "some_cluster", [])
+
+    assert not ensure_cluster_autoscaler_and_cluster_versions_match(resources)
