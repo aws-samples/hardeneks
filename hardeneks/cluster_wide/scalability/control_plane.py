d67397999fd6f2bb4fb22e452e32a1b992b8c8d1
Doruk Ozturk <dozturk@amazon.com>
2023-03-13 23:56:42 -0400

    feat: Make scalability section use the rule class

--- hardeneks/cluster_wide/scalability/control_plane.py
+++ hardeneks/cluster_wide/scalability/control_plane.py
@@ -1,49 +1,58 @@
 import re
 import kubernetes
-from rich.panel import Panel
 from hardeneks import helpers
-from hardeneks import console
+from hardeneks.rules import Rule, Result
 from hardeneks import Resources
 
 
-def check_EKS_version(resources: Resources):
-    client = kubernetes.client.VersionApi()
-    version = client.get_code()
-    minor = version.minor
+class check_EKS_version(Rule):
+    _type = "cluster_wide"
+    pillar = "scalability"
+    section = "control_plane"
+    message = "EKS Version Should be greater or equal to 1.24."
+    url = "https://aws.github.io/aws-eks-best-practices/scalability/docs/control-plane/#use-eks-124-or-above"
 
-    if int(re.sub("[^0-9]", "", minor)) < 24:
-        console.print(
-            Panel(
-                f"[red]EKS Version Should be greater or equal too 1.24. Current Version == {version.major}.{version.minor}",
-                subtitle="[link=https://aws.github.io/aws-eks-best-practices/scalability/docs/control-plane/#use-eks-124-or-above]Click to see the guide[/link]",
+    def check(self, resources: Resources):
+        client = kubernetes.client.VersionApi()
+        version = client.get_code()
+        minor = version.minor
+
+        if int(re.sub("[^0-9]", "", minor)) < 24:
+            self.result = Result(
+                status=False,
+                resources=f"{version.major}.{minor}",
+                resource_type="Cluster Version",
             )
-        )
-        console.print()
-        return False
+        else:
+            self.result = Result(status=True, resource_type="Cluster Version")
 
-    return True
 
 #
 # check_kubectl_compression
 # checks all clusters in config for disable-compression flag set to true
 # if any cluster does not have setting, it returns False
-def check_kubectl_compression(resources: Resources):
-    kubeconfig = helpers.get_kube_config()
-    isSetCorrectly = False
-    for cluster in kubeconfig.get("clusters", []):
-        clusterName = cluster.get("name", "")
-        if (resources.cluster in clusterName):
-            if cluster.get("cluster", {}).get("disable-compression", False) != True:
-                console.print(
-                    Panel(
-                        f"[red]`disable-compression` in Cluster {clusterName}  should equal True",
-                        subtitle="[link=https://aws.github.io/aws-eks-best-practices/scalability/docs/control-plane/#disable-kubectl-compression]Click to see the guide[/link]",
+class check_kubectl_compression(Rule):
+    _type = "cluster_wide"
+    pillar = "scalability"
+    section = "control_plane"
+    message = "`disable-compression` in kubeconfig should equal True"
+    url = "https://aws.github.io/aws-eks-best-practices/scalability/docs/control-plane/#disable-kubectl-compression"
+
+    def check(self, resources: Resources):
+        kubeconfig = helpers.get_kube_config()
+        for cluster in kubeconfig.get("clusters", []):
+            clusterName = cluster.get("name", "")
+            if resources.cluster in clusterName:
+                if not (
+                    cluster.get("cluster", {}).get(
+                        "disable-compression", False
                     )
-                )
-                console.print()
-            else:
-                isSetCorrectly = True
-            break
-        
-    
-    return isSetCorrectly
+                ):
+                    self.result = Result(
+                        status=False, resource_type="Compression Setting"
+                    )
+                else:
+                    self.result = Result(
+                        status=True, resource_type="Compression Setting"
+                    )
+                break
